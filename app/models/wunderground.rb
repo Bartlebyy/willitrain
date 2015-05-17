3bb5af843ced0bcf5996147d6ab9513564dce9d4
Diego Medina <d.medina1290@gmail.com>
2015-05-17 15:59:02 -0400

    Tweak report content

--- app/models/wunderground.rb
+++ app/models/wunderground.rb
@@ -13,7 +13,7 @@ class Wunderground
   end
 
   def run
-    open("http://api.wunderground.com/api/#{ENV['WUNDERGROUND_KEY']}/hourly/q/#{zipcode}.json") do |f|
+    open("http://api.wunderground.com/api/#{ ENV['WUNDERGROUND_KEY'] }/hourly/q/#{ zipcode }.json") do |f|
       json_string = f.read
       hourly_forecast = JSON.parse(json_string)['hourly_forecast']
       report(hourly_forecast)
@@ -21,14 +21,15 @@ class Wunderground
   end
 
   def report hourly_forecast, num_of_hours = 12
-    final_report = ["Hourly Report"]
+    final_report = ["\nWeather Update for #{ zipcode }"]
 
     (0...num_of_hours).map do |n| 
       data = hourly_forecast[n]
-      hour = data["FCTTIME"]["hour_padded"]
+      hour = data["FCTTIME"]["civil"].rjust(8)
       temp = data["temp"]["english"].rjust(2) + "°"
-      probability_of_precipitation = data["pop"].rjust(2) + "%"
-      final_report << "#{ hour }: #{ temp } | #{ probability_of_precipitation }"
+      rain = data["pop"].rjust(2) + "%"
+      condition = data["condition"]
+      final_report << "#{ hour }: #{ temp } | rain: #{ rain } | #{ condition }"
     end
     final_report.join("\n")
   end
